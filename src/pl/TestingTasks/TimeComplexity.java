191f8e843971d724a8a70852f2d070bb3c31c3eb
majkel <michal.sobotka.84@gmail.com>
2019-12-02 23:03:08 +0100

    Perm CheckMissing

--- src/pl/TestingTasks/TimeComplexity.java
+++ src/pl/TestingTasks/TimeComplexity.java
@@ -1,9 +1,28 @@
 package pl.TestingTasks;
 
+import java.util.Arrays;
+
 public class TimeComplexity {
 
 	public int FrogJump(int X, int Y, int D) {
 		int solution = Y - X;
 		return solution % D == 0 ? solution /= D : (solution /= D) + 1;
+	}
+	
+	public int PermMissingElem(int[] A) {
+		if (A.length == 0)
+			return 1;
+		Arrays.sort(A);
+		if (A[0]!=1)
+			return 1;
+		if (A.length == A[A.length-1])
+			return A.length + 1;
+	    if (A.length == 1 && A[0] != 1)
+	        return 1;
+			    for (int i = 0; i < A.length - 1; i++)
+	        if (A[i+1] - A[i] != 1)
+	            return i+2;
+		System.out.println(A.length);
+		return 0;
 	}
 }
--- test/pl/TestingTasks/TimeComplexityTest.java
+++ test/pl/TestingTasks/TimeComplexityTest.java
@@ -25,5 +25,26 @@ class TimeComplexityTest {
 		int result = tester.FrogJump(3, 1000000000, 3);
 		assertEquals(333333333, result);
 	}
+	
+	@Test
+	void checkVectorWithGaps() {
+		int[] A = new int[]{2, 3, 1, 6, 4};
+		int result = tester.PermMissingElem(A);
+		assertEquals(5, result);
+	}
+	
+	@Test
+	void checkVectorNoGaps() {
+		int[] A = new int[]{2, 3, 1, 5, 4};
+		int result = tester.PermMissingElem(A);
+		assertEquals(6, result);
+	}
+	
+	@Test
+	void checkEmptyVector() {
+		int[] A = new int[]{0};
+		int result = tester.PermMissingElem(A);
+		assertEquals(1, result);
+	}
 
 }
